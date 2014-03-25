d8de26e7b27d9d1bf1e79530fe5726dd439832cd
Aaron Sharp <asahraornp@gmail.com>
2014-03-25 12:26:59 -0600

    Initialized project.  Hello, World message works.

---
+++ ThoughtScape/CentralController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThoughtScape
+{
+    class CentralController
+    {
+        public static void Main()
+        {
+            System.Console.WriteLine("Hello, World");
+        }
+    }
+}
+
