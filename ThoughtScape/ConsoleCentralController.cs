8a4e589be1355f077ffd7c48a3ae14c1737bddbc
Aaron Sharp <asahraornp@gmail.com>
2014-03-31 11:18:06 -0600

    Provided (very) basic user interaction.

--- ThoughtScape/ConsoleCentralController.cs
+++ ThoughtScape/ConsoleCentralController.cs
@@ -4,9 +4,33 @@ namespace ThoughtScape
 {
     class ConsoleCentralController : CentralController
     {
+        private bool VarShouldContinue = true;
+
         public override void OfferMenu()
         {
-            Console.WriteLine("Hello, World");
+            while (this.ShouldContinue())
+            {
+                Console.WriteLine("WHAT WOULD YOU LIKE TO DO?\n\t1. CREATE A NEW NOTE\n\t2. QUIT");
+                string InputOption = Console.ReadLine();
+                if ("1".Equals(InputOption)) {
+                    Console.WriteLine("UNFORTUNATELY, THAT OPTION IS NOT YET IMPLEMENTED.");
+                }
+                else if ("2".Equals(InputOption)) {
+                    this.SetShouldContinue(false);
+                }
+                else {
+                    Console.WriteLine("THAT IS NOT A VALID OPTION: {0}", InputOption);
+                }
+            }
+        }
+
+        private bool ShouldContinue()
+        {
+            return this.VarShouldContinue;
+        }
+        private void SetShouldContinue(bool ShouldContinue)
+        {
+            this.VarShouldContinue = ShouldContinue;
         }
     }
 }
