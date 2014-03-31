905762e256f1d7f6fda99d7a8c599da9fc033ae1
Aaron Sharp <asahraornp@gmail.com>
2014-03-31 10:33:23 -0600

    Created an abstract controller superclass.

--- ThoughtScape/CentralController.cs
+++ ThoughtScape/CentralController.cs
@@ -1,17 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ThoughtScape
 {
-    class CentralController
+    abstract class CentralController
     {
         public static void Main()
         {
-            System.Console.WriteLine("Hello, World");
+            CentralController controller = new ConsoleCentralController();
+            controller.OfferMenu();
         }
+
+        public abstract void OfferMenu();
     }
 }
 
---
+++ ThoughtScape/ConsoleCentralController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThoughtScape
+{
+    class ConsoleCentralController : CentralController
+    {
+        public override void OfferMenu()
+        {
+            Console.WriteLine("Hello, World");
+        }
+    }
+}
